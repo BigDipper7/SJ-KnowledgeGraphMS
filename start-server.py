c91f61bf6217d105e09e9ed9f8034d93eaa27463
Minghao Xu <violiniselegant@gmail.com>
2016-01-06 21:01:06 +0800

    omit useless code block and add start server code

---
+++ start-server.py
@@ -0,0 +1,10 @@
+from webapp import app
+
+
+
+
+#----------------------------------------
+# main func snippet
+#----------------------------------------
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host="0.0.0.0")
