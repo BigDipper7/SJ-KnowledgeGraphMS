3b01edf6570caef083d0b5d138dc7cb09a817989
Minghao Xu <violiniselegant@gmail.com>
2016-01-06 14:49:43 +0800

    add UT in travis

--- test-all.py
+++ test-all.py
@@ -1,7 +1,16 @@
 import pytest
+from tj.util.file.file_util import create_file_with_time, _create_dir_with_time
 
 def funct(a):
     return a+1
 
 def test_all():
     assert funct(1)==2
+
+def test_file_util_funcs():
+    root_path = 'test'
+    filename = "test.md"
+    final_dir = create_file_with_time(root_path, filename)
+    temp_dir = _create_dir_with_time(root_path)
+    assert final_dir.startswith(root_path+'/')
+    assert final_dir.endswith('/'+filename)
