37946d4cb45ecfd3d2b75474dcbd684d72154e17
Minghao Xu <violiniselegant@gmail.com>
2016-01-07 10:52:49 +0800

    fix typo in test-all because of the import error

--- webapp/test/test-all.py
+++ webapp/test/test-all.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from businesslogic.util.file.file_util import create_file_with_time, _create_dir_with_time
+from webapp.businesslogic.util.file.file_util import create_file_with_time, _create_dir_with_time
 
 def funct(a):
     return a+1
