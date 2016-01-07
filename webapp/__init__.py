c02aa7f8db0ff7cb5a11277ade1b662aa2b32c63
Minghao Xu <violiniselegant@gmail.com>
2016-01-07 11:50:48 +0800

    add annotation

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -10,6 +10,9 @@ from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'jiu bu gao su ni'
+# TODO: 注意的是这里的“.”指的是当前路径，而这里的当前路径并不是init_py的路径，
+# 而是应该是运行的程序“start-server.py”所在的路径，
+# 因为整个程序的调用顺序是start-server -> webapp.__init__.app -> import webapp.views.views -> import dependencies
 app.config['UPLOAD_FOLDER'] = './webapp/FileUpload'
 bootstrap = Bootstrap(app)
 manager = Manager(app)
