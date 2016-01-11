4f3ce3796b2d80a67da2f525e1845f8f0fe4ef08
Minghao Xu <violiniselegant@gmail.com>
2016-01-11 16:23:32 +0800

    add framework

--- MongoDB/test/MongoDBMain.py
+++ MongoDB/test/MongoDBMain.py
@@ -2,11 +2,24 @@ from pymongo import MongoClient
 
 MDB_URL = 'mongodb://127.0.0.1:27017'
 MDB_NAME = 'cayley'
+MDB_COLLECTION_LOG = 'log'
+MDB_COLLECTION_QUADS = 'quads'
+MDB_COLLECTION_NODES = 'nodes'
 
+#get mongo client, guess same as mongo
 client =  MongoClient(MDB_URL)
+#get specified db, just like use cayley
 db = client[MDB_NAME]
 
-collection = db.collection
+# collection = db.collection
+
+def main():
+    #get three main collections through name and db
+    collecion_quads = db[MDB_COLLECTION_QUADS]
+    collecion_nodes = db[MDB_COLLECTION_NODES]
+    collecion_log = db[MDB_COLLECTION_LOG]
+
+    return
 
 if __name__ == '__main__':
     main()
