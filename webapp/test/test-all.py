6488ea3fc92ea5d3c4b5bf59b5530feeba1afd7e
Minghao Xu <violiniselegant@gmail.com>
2016-01-22 11:48:15 +0800

    update test method, update UT, make build success

--- webapp/test/test-all.py
+++ webapp/test/test-all.py
@@ -25,12 +25,26 @@ def test_file_util_funcs():
     print "temp_dir:{}".format(temp_dir)
 
 def test_import_util():
-    a = 'foofoofoo\nfoofoo'
-    b = 'foofoofoo\\nfoofoo'
-    g = _str_pre_process(a)
-    assert b == g
+    # a = 'foofoofoo\nfoofoo'
+    # b = 'foofoofoo\\nfoofoo'
+    # g = _str_pre_process(a)
+    # assert b == g
+    #
+    # c = 'foo\n\nfooofoobar'
+    # d = 'foo\\n\\nfooofoobar'
+    # e,f = _str_pre_process(a,c)
+    # assert e==b and f==d
+    a = '  shdjsdh '
+    b = 'shdjsdh'
+    c = _str_pre_process(False, a)
+    assert c == b
 
-    c = 'foo\n\nfooofoobar'
-    d = 'foo\\n\\nfooofoobar'
-    e,f = _str_pre_process(a,c)
-    assert e==b and f==d
+    e = '\n\n\n12nkasdui234\n \t'
+    f = '12nkasdui234'
+    g = _str_pre_process(False, e)
+    assert g == f
+
+    h = 'Lore&nbsp;m ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    i = 'Lore m ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    j = _str_pre_process(True, h)
+    assert j == i
